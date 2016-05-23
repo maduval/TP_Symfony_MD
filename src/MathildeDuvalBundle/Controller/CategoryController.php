ede1121699e6b5be9916eff1e8a37ce1e249e9d9
Mathilde Duval <duvalmathilde@hotmail.fr>
2016-05-23 10:15:49 +0200

    Pagination fonctionnelle

--- src/MathildeDuvalBundle/Controller/CategoryController.php
+++ src/MathildeDuvalBundle/Controller/CategoryController.php
@@ -15,10 +15,10 @@ class CategoryController extends Controller
 {
     /**
      *
-     * @Route("/{slug}", name="md_category_show")
+     * @Route("/{slug}/{page}", name="md_category_show", defaults={"page" = 1})
      * @Method("GET")
      */
-    public function showAction($slug)
+    public function showAction($slug, $page)
     {
         $em = $this->getDoctrine()->getEntityManager();
 
@@ -28,10 +28,21 @@ class CategoryController extends Controller
             throw $this->createNotFoundException('Unable to find Category entity.');
         }
 
-        $category->setActiveJobs($em->getRepository('MathildeDuvalBundle:Job')->getActiveJobs($category->getId()));
+        $total_jobs = $em->getRepository('MathildeDuvalBundle:Job')->countActiveJobs($category->getId());
+        $jobs_per_page = $this->container->getParameter('max_jobs_on_category');
+        $last_page = ceil($total_jobs / $jobs_per_page);
+        $previous_page = $page > 1 ? $page - 1 : 1;
+        $next_page = $page < $last_page ? $page + 1 : $last_page;
+
+        $category->setActiveJobs($em->getRepository('MathildeDuvalBundle:Job')->getActiveJobs($category->getId(), $jobs_per_page, ($page - 1) * $jobs_per_page));
 
         return $this->render('category/show.html.twig', array(
             'category' => $category,
+            'last_page' => $last_page,
+            'previous_page' => $previous_page,
+            'current_page' => $page,
+            'next_page' => $next_page,
+            'total_jobs' => $total_jobs
         ));
     }
 
