f3c16785c16444bc447b934957be5808c23edf0d
Mathilde Duval <duvalmathilde@hotmail.fr>
2016-05-23 10:33:04 +0200

    Premier test unitaire

---
+++ src/MathildeDuvalBundle/Tests/Utils/JobeetTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MathildeDuvalBundle\Tests\Utils;
+use MathildeDuvalBundle\Utils\Jobeet;
+
+class JobeetTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSlugify()
+    {
+    $this->assertEquals('sensio', Jobeet::slugify('Sensio'));
+    $this->assertEquals('sensio-labs', Jobeet::slugify('sensio labs'));
+    $this->assertEquals('sensio-labs', Jobeet::slugify('sensio   labs'));
+    $this->assertEquals('paris-france', Jobeet::slugify('paris,france'));
+    $this->assertEquals('sensio', Jobeet::slugify('  sensio'));
+    $this->assertEquals('sensio', Jobeet::slugify('sensio  '));
+    }
+}
