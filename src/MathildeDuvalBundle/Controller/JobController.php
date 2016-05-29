a337eb464b8ec52669ec1f01e4ccba45f46eb39e
Mathilde Duval <duvalmathilde@hotmail.fr>
2016-05-29 15:31:22 +0200

    Fin securite

--- src/MathildeDuvalBundle/Controller/JobController.php
+++ src/MathildeDuvalBundle/Controller/JobController.php
@@ -94,15 +94,36 @@ class JobController extends Controller
      * @Route("/{company}/{location}/{id}/{position}", requirements={"id" = "\d+"}, name="md_job_show")
      * @Method("GET")
      */
-    public function showAction(Job $job)
+    public function showAction($id)
     {
-        $deleteForm = $this->createDeleteForm($job);
+        $em = $this->getDoctrine()->getEntityManager();
 
-        $em = $this->getDoctrine()->getManager();
-        $jobChecked = $em->getRepository('MathildeDuvalBundle:Job')->getActiveJob($job->getId());
+        $entity = $em->getRepository('MathildeDuvalBundle:Job')->getActiveJob($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Job entity.');
+        }
+
+        $session = $this->getRequest()->getSession();
+
+        // fetch jobs already stored in the job history
+        $jobs = $session->get('job_history', array());
+
+        // store the job as an array so we can put it in the session and avoid entity serialize errors
+        $job = array('id' => $entity->getId(), 'position' =>$entity->getPosition(), 'company' => $entity->getCompany(), 'companyslug' => $entity->getCompanySlug(), 'locationslug' => $entity->getLocationSlug(), 'positionslug' => $entity->getPositionSlug());
+
+        if (!in_array($job, $jobs)) {
+            // add the current job at the beginning of the array
+            array_unshift($jobs, $job);
+
+            // store the new job history back into the session
+            $session->set('job_history', array_slice($jobs, 0, 3));
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('job/show.html.twig', array(
-            'job' => $jobChecked,
+            'job'      => $entity,
             'delete_form' => $deleteForm->createView(),
         ));
     }
